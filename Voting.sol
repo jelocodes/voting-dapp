26e800000abadc6d6553cedaf63e4382d25ddee2
Michaelangelo Yambao <michaelangelo.yambao@gmail.com>
2017-10-25 22:21:17 -0400

    create mapping of candidates to number of votes, as well as separate array of candidate names

--- Voting.sol
+++ Voting.sol
@@ -1,6 +1,9 @@
 pragma solidity ^0.4.11;
 
 contract smartVoting {
+	// maps candidates (bytes32) with number of votes recived (uint8)
+	mapping (bytes32 => uint8) public votesReceived;
 
-
+	// a separate array of candidate names, since Solidity lacks a .keys method
+	bytes32[] public candidateList;
 }
